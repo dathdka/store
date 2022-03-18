3f51fb57d790a2eda275b164abfad9e9a760cf04
dathdka <dathdka@gmail.com>
2022-03-18 18:25:52 +0700

    Hoàn thiện tìm kiếm - Hiển thị sản phẩm có giảm giá

--- KhachHang/sanPham.php
+++ KhachHang/sanPham.php
@@ -1,19 +1,28 @@
 <?php
 require_once("../KhachHang/layout/header.php");
 require_once("../entities/sanPham.class.php");
+require_once("../entities/hangSX.class.php");
 require_once("../config/db.class.php");
 include("../admin/permission.php");
 $dSSP = sanPham::dSSP();
-if(isset($_GET["btnXacNhan"])){
-    $timKiem = "SELECT * FROM sanpham WHERE    ";
-    $timKiem .=isset($_GET["cbNam"]) ? "GioiTinh=0     AND ":"";
-    $timKiem .=isset($_GET["cbNu"]) ? "GioiTinh=1     AND ":"";
-    isset($_GET["cbKM"]) ?$timKiem .= "KhuyenMai>0 ":$timKiem = substr($timKiem, 0,-9);
-    $timKiem .=($_GET["sbGia"]=="giam") ? " ORDER BY DonGia DESC": " ORDER BY DonGia";
-    echo $timKiem;
+$timKiem = "SELECT * FROM sanpham WHERE ";
+if(isset($_GET['timKiem'])){
+    $keyWord = $_GET['timKiem'];
+    $timKiem .=  " TenSP LIKE  '%$keyWord' OR TenSP LIKE  '$keyWord%'";
+    echo "<h1>Kết quả tìm kiếm: ". $keyWord ."</h1>";
     $dSSP = sanPham::dSSPTK($timKiem);
 }
+if(isset($_GET["btnXacNhan"])){
+    $timKiem .=($_GET["cbHSX"]==0) ? "" : " MaNSX=".$_GET["cbHSX"]."  AND ";
+    $timKiem .=isset($_GET["cbNam"]) ? " GioiTinh=0  AND ":"";
+    $timKiem .=isset($_GET["cbNu"]) ? " GioiTinh=1  AND ":"";
+    isset($_GET["cbKM"]) ?$timKiem .= " KhuyenMai>0 ":$timKiem = substr($timKiem, 0,-6);
+    $timKiem .=($_GET["sbGia"]=="giam") ? " ORDER BY DonGia DESC": " ORDER BY DonGia";
+    $dSSP = sanPham::dSSPTK($timKiem);
+    
+}
 
+echo $timKiem;
 ?>
 <form method="GET">
     <h3>Giá: </h3>
@@ -21,12 +30,23 @@ if(isset($_GET["btnXacNhan"])){
         <option value="tang" <?php echo (isset($_GET["sbGia"]) && $_GET["sbGia"]== "tang") ? "selected=true" : ""; ?>>Tăng</option>
         <option value="giam" <?php echo (isset($_GET["sbGia"]) && $_GET["sbGia"]== "giam") ? "selected=true" : ""; ?>>Giảm</option>
     </select>
+    <h3>Hãng sản xuất: </h3>
+    <select name="cbHSX">
+        <option value="0" selected>Tất cả</option>
+        <?php
+        $HSX = hangSX::dSHSX();
+        foreach ($HSX as $item) {
+            echo "<option value=" . $item["MaNSX"] . ">" . $item["TenNSX"] . "</option>";
+        }
+        ?>
+    </select>
         <h3>Nam: </h3>
         <input type="checkbox" name="cbNam" <?php echo isset($_GET["cbNam"]) ? "checked=true" : ""; ?>>
         <h3>Nữ: </h3>
         <input type="checkbox" name="cbNu" <?php echo isset($_GET["cbNu"]) ? "checked=true" : ""; ?>>
         <h3>Khuyến mãi: </h3>
         <input type="checkbox" name="cbKM" <?php echo isset($_GET["cbKM"]) ? "checked=true" : ""; ?>>
+    
         <input type="submit" name="btnXacNhan" value="Xác nhận">
 </form>
 <?php
@@ -36,11 +56,19 @@ foreach ($dSSP as $item) {
     <div class="col-md-6">
         <h1><?php echo $item["MaSP"] ?></h1>
         <h1><?php echo $item["TenSP"] ?></h1>
+        <?php if($item["KhuyenMai"]>0) {
+            ?>
+                <h1 style="text-decoration: line-through;"><?php echo $item["DonGia"] ?></h1>
+                <h1><?php echo $item["DonGia"] - ($item["KhuyenMai"]* $item["DonGia"])/100 ?></h1>
+        <?php    }
+        else{ ?>
         <h1><?php echo $item["DonGia"] ?></h1>
+        <?php }
+        ?>
         <h1><?php echo ($item["GioiTinh"] == "0") ? "Nam" : "Nữ"; ?></h1>
         <h1><?php echo $item["MoTa"] ?></h1>
         <h1><?php echo $item["SoLuong"] ?></h1>
-        <h1><?php echo $item["KhuyenMai"] ?></h1>
+        <h1 style="line-through:true"><?php echo $item["KhuyenMai"] ?></h1>
         <img src="<?php echo $item["HinhAnh"] ?>" style="width:100px">
         <form method="post" onsubmit="return false" name="form">
             <button type="submit" id=<?php echo $item["MaSP"]; ?> onclick="changevalue(<?php echo $item['MaSP'] ?>);">
@@ -78,32 +106,5 @@ foreach ($dSSP as $item) {
             xmlhttp.send();
         }
     };
-    //Sắp xếp
-    // function gia(){
-    //     var x = document.getElementById("jsGia");
-    //     if(x.innerHTML == )
-    // }
-    // function gioiTinh(){
-    //     var x = document.getElementById("jsGioiTinh");
-    //     if(x.innerHTML != "Giới tính: Nam"){
-    //         x.innerHTML = "Giới tính: Nam";
-    //         xmlhttp.open("GET", "sanPham.php?GioiTinh=Nam", true);
-    //         xmlhttp.send();
-    //     }
-    //     else{
-    //         x.innerHTML = "Giới tính: Nữ";
-    //     }
-    //     x.style.backgroundColor="blue";
-    // }
-    // function KM(){
-    //     var x = document.getElementById("jsKM");
-    //     if(x.style.backgroundColor!="blue"){
-    //         x.style.backgroundColor="blue";
-    //     }
-    //     else{
-    //         x.style.backgroundColor="white";
-    //     }
-    // }
-    // function xacNhan(){
-    // }
+   
 </script>
